b3e3123bcabc3ac451af855ce49647b28f04918e
OmarCode-1 <omar.programing.0.0122@gmail.com>
2025-12-14 16:34:32 +0200

    V1 4.34

--- classes/main.py
+++ classes/main.py
@@ -1,23 +1,31 @@
 class Product:
-    ...
+    def __init__(self, prod_id: int, prod_name: str, prod_price: int, prod_desc: str)-> None:
+        self.id = prod_id
+        self.prod_name = prod_name
+        self.prod_price = prod_price
+        self.prod_desc = prod_desc
 
-class CartItem:
-    ...
+# """
+#     id : {
+#         obj : object,
+#         Qu:
+#     }
 
+# """
 class Cart:
     def __init__(self):
-        self.items = []
+        self.items = {}
 
-    def get_cart():
+    def getCart(self):
         ...
 
-    def add_product(prod: Product, quantity: int = 1):
+    def addProduct(self, prod: Product, quantity: int = 1):
+        if self.items[prod.id]
+
+    def removeProduct(self, prod: Product, quantity: int = 1):
         ...
 
-    def remove_product(prod: Product, quantity: int = 1):
-        ...
-
-    def checkout():
+    def checkout(self):
         #return reciept info
         #create reciept
         ...
