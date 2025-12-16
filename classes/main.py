8a91e528d77d4f7242895feeb6cafbb9f8bc661d
AbdelrahmanMostafaSamy <abdelrahmanmostafasamy@gmail.com>
2025-12-16 17:07:33 +0200

    a

--- classes/main.py
+++ classes/main.py
@@ -8,170 +8,6 @@ class Product:
         self.price  = prod_price
         self.desc   = prod_desc
 
-
-
-class Stock:
-    def __init__(self):
-        """
-        Docstring for __init__
-        
-        :param self: Self instance attribute
-        """
-        """
-            self.products the products which you have in the store and their quantity 
-            
-            product: dict {
-                id : {
-                    "obj"       : Object of the product.
-                    "quantity"  : Quantity you have it in th stock. 
-                }
-            }
-
-        """
-        
-        # basic stock products .
-        self.products: dict = {
-            101: {
-                "obj" : Product(101, "Milk", 300, "1L of milk."),
-                "Quantity" : 25
-            },
-
-            102: {
-                "obj": Product(102, "Bread", 250, "White bread loaf."),
-                "Quantity" : 25
-            },
-
-            103: {
-                "obj": Product(103, "Eggs", 450, "12 eggs."),
-                "Quantity" : 25
-            },
-
-            104: {
-                "obj": Product(104, "Butter", 500, "Butter stick."),
-                "Quantity" : 25
-            },
-
-            105: {
-                "obj": Product(105, "Soap", 199, "Hand soap."),
-                "Quantity" : 25
-            }
-        }
-
-    def addProductToStock(self, product: Product = None, quantity: int = 0) -> bool:
-        """
-            Docstring for addProductToStock
-            
-            :param self: self instance attribute
-            :param product: Object from the product class I need to add it into the stock
-            :type product: Product
-            :param quantity: Quantity I have it in the stock.
-            :type quantity: int
-            :return: True if the process has finished successfully. 
-            :rtype: bool
-        """
-
-        # If the object doesn't exists.
-        if Product == None:
-            return False
-
-        # add the product.
-        self.products[product.id] = {
-            "obj": product,
-            "Quantity" : quantity
-        }
-
-        return True
-
-    def deleteFromStock(self, id_of_product : int) -> bool:
-        """
-        Docstring for deleteFromStock
-        
-        :param self: self instance attribute.
-        :param id_of_product: Id that the product have.
-        :type id_of_product: int
-        :return: True if the process has finished successfully.
-        :rtype: bool
-        """
-
-        if id_of_product in self.products.keys():
-            self.products.pop(id_of_product)
-            return True
-        else:
-            return False
-
-    def updateProduct(self, id_of_product: int, name: str = "", price: int = 0, desc: str = "") -> bool:
-        """
-        Docstring for updateProduct
-        
-        :param self: self instance attribute
-        :param name: name you want to update the product.
-        :type name: str
-        :param price: Price you want to update it.
-        :type price: int
-        :param desc: Description you want to update it.
-        :type desc: str
-        :return: True if the process has finished successfully.
-        :rtype: bool
-        """
-        # Check if the id of product , already i have or not.
-        if id_of_product in self.products.keys():
-            if name != "":
-                # update the name of product if the user entered value.
-                self.products[id_of_product]["obj"].name = name
-            
-            if price != 0:
-                # update the price of product if the user entered value.
-                self.products[id_of_product]["obj"].price = price
-
-            if desc != "":
-                # update the desc of product if the user entered value.
-                self.products[id_of_product]["obj"].desc = desc
-            
-            # True if the process has finished successfully.
-            return True
-        
-        else:
-            # False if the process has finished with failed.
-            return False
-
-    def get_quantity(self, id_of_product: int) -> int:
-        """
-        Docstring for get_quantity
-        
-        :param self: Self instance attribute
-        :param id_of_product: id of the product which I need to get its quantity in the stock.
-        :type id_of_product: int
-        :return: the quantity we have it from this product in the stock.
-        :rtype: int
-        """
-        # if I have the product in My stock.
-        if id_of_product in self.products.keys():
-            # return its quantity.
-            return self.products[id_of_product]["Quantity"]
-        else:
-            # if I don't it return -1.
-            return -1
-        
-    def set_quantity(self, id_of_product: int, new_quantity: int) -> bool:
-        """
-        Docstring for set_quantity
-        
-        :param self: Self instance attribute
-        :param id_of_product: id of the product which I need to get its quantity in the stock.
-        :type id_of_product: int
-        :param new_quantity: The new quantity I need to set it.
-        :type new_quantity: int
-        :return: True if the process has finished successfully.
-        :rtype: bool
-        """
-        # if I have the product in My stock.
-        if id_of_product in self.products.keys():
-            # set new quantity quantity.
-            self.products[id_of_product]["Quantity"] = new_quantity
-            return True
-        else:
-            # if I don't have it return false.
-            return False
         
 class Cart:
     def __init__(self):
@@ -324,13 +160,13 @@ class Cart:
         return msgs 
 
 
-# LISTOFPRODUCTS = [
-#     Product(101, "Milk", 300, "1L of milk."),
-#     Product(102, "Bread", 250, "White bread loaf."),
-#     Product(103, "Eggs", 450, "12 eggs."),
-#     Product(104, "Butter", 500, "Butter stick."),
-#     Product(106, "Soap", 199, "Hand soap.")
-# ]
+LISTOFPRODUCTS = [
+    Product(101, "Milk", 300, "1L of milk."),
+    Product(102, "Bread", 250, "White bread loaf."),
+    Product(103, "Eggs", 450, "12 eggs."),
+    Product(104, "Butter", 500, "Butter stick."),
+    Product(106, "Soap", 199, "Hand soap.")
+]
 
 
 # my_cart = Cart()
